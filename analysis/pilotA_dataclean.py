14a765746d19579562aa1a6836da6cbf844eae63
Nastasia Klevak <nastasia.k1214@gmail.com>
2023-11-09 12:54:45 -0800

    adding processed data

--- analysis/pilotA_dataclean.py
+++ analysis/pilotA_dataclean.py
@@ -9,11 +9,11 @@ import glob
 
 def extract_data():
     # gets all filenames for subject data
-    path = os.path.abspath("pilotA")
+    path = os.path.abspath("data")
     sys.path.append(path)
-    print(sys.path)
+    print(path)
     # abs_path = "./data/pilotA/"
-    subjectfiles = ["pilotA-rescue-251_1.csv", "pilotA-rescue-251_3.csv"]
+    subjectfiles = ["/pilotA/pilotA-rescue-251_1.csv", "/pilotA/pilotA-rescue-251_3.csv"]
     # for file in glob.glob("*pilotA-rescue-251_*.csv"):
     #     subjectfiles.append(file)
     #     print(file)
@@ -23,8 +23,8 @@ def extract_data():
     
     # iterate over each subject (file)
     for filename in subjectfiles:
-        # fullname = sys.path + filename
-        df_curr = pd.read_csv (filename)
+        fullname = path + filename
+        df_curr = pd.read_csv (fullname)
         print(df_curr)
         subject_trial_data = pd.concat([subject_trial_data,df_curr])
     
@@ -33,21 +33,37 @@ def extract_data():
 # extract and format data from database
 data = extract_data()
 
-# data['positive_key'] = np.nan
-# data['negative_key'] = np.nan
-# data['association'] = np.nan
+data['positive_key'] = np.nan
+data['negative_key'] = np.nan
+data['association'] = np.nan
 
-# mapping = {'p':80, 'q':81, 'space':32}
+mapping = {'p':80, 'q':81, 'space':32}
 
-# for i_subject in data.subject.unique(): 
-#     for i_valence in ['positive', 'negative']: 
+print(data.run_id.unique())
+for i_subject in data.run_id.unique(): 
+    for i_valence in ['positive', 'negative']: 
         
-#         conditions = (data.subject==i_subject) * (data.valence==i_valence) * (data.condition=='instrumental')
-#         # get the key value and map it to the numerical value
-#         i_key = mapping[np.unique(data[conditions]['correct_response'])[0]]
+        # conditions = (data.run_id==i_subject) * (data.valence==i_valence) * (data.stage=='instrumental')
+        # print(data.run_id==i_subject)
+        # print(data.valence==i_valence)
+        # print(data.stage=='instrumental')
+        # print("conditions:")
+        # print(conditions)
+        # print("conditions rows:")
+        if (i_subject != 1 & i_valence != 'positive'):
+            "test"
+            continue
+        print(data[data.run_id==i_subject])
+        # print(data[conditions])
+        # # get the key value and map it to the numerical value
+        # if data[conditions].empty:
+        #     print("df is empty?")
+        #     continue
+        # else:
+        #     i_key = mapping[np.unique(data[conditions]['correct_response'])[0]]
         
-#         # update subject valence-key mapping 
-#         data['%s_key'%i_valence][data.subject==i_subject] = int(i_key)
+        # # update subject valence-key mapping 
+        # data['%s_key'%i_valence][data.subject==i_subject] = int(i_key)
         
-# # save for later analysis
-# data.to_csv('./data/pilotA/subject_data_pilotA_processed.csv')
+# save for later analysis
+data.to_csv('./data/pilotA/subject_data_pilotA_processed.csv')
